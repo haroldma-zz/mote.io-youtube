203acf1ebf7f33717407eb2532c22848c409e4ad
Ian Jennings <ian@meetjennings.com>
2013-09-25 13:21:05 -0400

    basic playlist management

--- assets/js/leanback.js
+++ assets/js/leanback.js
@@ -1,25 +1,87 @@
-var apiKey = "AIzaSyCkczmIB0LJZeoXjwYvVNHlD4asew7zBb4";
+var apiKey = "AIzaSyCkczmIB0LJZeoXjwYvVNHlD4asew7zBb4",
+	perPage = 10,
+	requests = [
+	{
+		name: 'Trending',
+		ajax: {
+	    url: 'https://www.googleapis.com/youtube/v3/search',
+	    data: {
+	      part: 'snippet',
+	      order: 'rating',
+	      safeSearch: 'none',
+	      type: 'video',
+	      videoDimension: '2d',
+	      videoEmbeddable: true,
+	      videoSyndicated: true,
+	      maxResults: perPage,
+	      key: apiKey
+	    }
+	  }
+	},
+	{
+		name: 'Most Popular',
+		ajax: {
+	    url: 'https://www.googleapis.com/youtube/v3/videos',
+	    data: {
+	      part: 'snippet',
+	      chart: 'mostPopular',
+	      maxResults: perPage,
+	      key: apiKey
+	    }
+	  }
+	},
+	{
+		name: 'Music',
+		ajax: {
+	    url: 'https://www.googleapis.com/youtube/v3/search',
+	    data: {
+	      part: 'snippet',
+	      order: 'rating',
+	      safeSearch: 'none',
+	      type: 'video',
+	      videoDimension: '2d',
+	      videoEmbeddable: true,
+	      videoSyndicated: true,
+	      topId: '/music',
+	      maxResults: perPage,
+	      key: apiKey
+	    }
+	  }
+	},
+];
+
+var player = null;
+
 $(function() {
-  $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    data: {
-      part: 'snippet',
-      order: 'rating',
-      safeSearch: 'none',
-      type: 'video',
-      videoDimension: '2d',
-      videoEmbeddable: true,
-      videoSyndicated: true,
-      maxResults: 10,
-      key: apiKey
-    }
-  })
+
+	var params = { allowScriptAccess: "always" };
+    var atts = { id: "ytapiplayer" };
+    swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&version=3",
+                       "ytapiplayer", "100%", "100%", "8", null, null, params, atts);
+
+});
+
+function onYouTubePlayerReady(playerId) {
+
+	var playlist = [];
+	player = document.getElementById('ytapiplayer');
+
+  $.ajax(requests[1].ajax)
   .done(function(data) {
-    console.log(data)
+
+  	console.log(player)
+
+  	for(var i = 0; i < data.items.length; i++) {
+	  	playlist.push(data.items[i].id);
+  	}
+  	player.loadPlaylist(playlist);
+
   })
   .fail(function() {
     alert( "error" );
   })
   .always(function() {
+
   });
-});
+
+}
